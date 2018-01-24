d442b451e032d3fdd96a8ae47845ec5b0dc80aa2
zanshichun <zanshichun@163.com>
2018-01-24 21:02:33 +0800

    change ssh default port

---
+++ change_ssh_default_port.sh
@@ -0,0 +1,99 @@
+#! /bin/bash
+
+sshd_config_dir=`find / -name sshd_config`
+check_port=`cat $sshd_config_dir |grep 'Port'`
+read -p "plz input a number to sshd_Port" Port
+if [ $Port -ge 1 ] && [ $Port -le 65535 ]; then
+  echo $Port
+else
+  echo 'plz input 1-65535 number!'
+  read -p "plz input a number to sshd_Port" Port
+  if [ $Port -ge 1 ] && [ $Port -le 65535 ]; then
+    echo $Port
+  else
+    echo 'plz input 1-65535 number!'
+    exit
+fi
+
+function checkos(){
+    if [ -f /etc/redhat-release ];then
+        OS=CentOS
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS=Debian
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS=Ubuntu
+    else
+        echo "Not support OS, Please reinstall OS and retry!"
+        exit 1
+    fi
+}
+# Get version
+function getversion(){
+    if [[ -s /etc/redhat-release ]];then
+        grep -oE  "[0-9.]+" /etc/redhat-release
+    else
+        grep -oE  "[0-9.]+" /etc/issue
+    fi
+}
+
+# CentOS version
+function centosversion(){
+    local code=$1
+    local version="`getversion`"
+    local main_ver=${version%%.*}
+    if [ $main_ver == $code ];then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+#防火墙设置
+function firewall_set(){
+    echo "firewall set start..."
+    if centosversion 6; then
+        /etc/init.d/iptables status > /dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            iptables -L -n | grep '${Port}' | grep 'ACCEPT' > /dev/null 2>&1
+            if [ $? -ne 0 ]; then
+                iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport ${Port} -j ACCEPT
+                iptables -I INPUT -m state --state NEW -m udp -p udp --dport ${Port} -j ACCEPT
+                /etc/init.d/iptables save
+                /etc/init.d/iptables restart
+            else
+                echo "port ${Port} has been set up."
+            fi
+        else
+            echo "WARNING: iptables looks like shutdown or not installed, please manually set it if necessary."
+        fi
+    elif centosversion 7; then
+        systemctl status firewalld > /dev/null 2>&1
+        if [ $? -eq 0 ];then
+            firewall-cmd --permanent --zone=public --add-port=${Port}/tcp
+            firewall-cmd --permanent --zone=public --add-port=${Port}/udp
+            firewall-cmd --reload
+        else
+            echo "Firewalld looks like not running, try to start..."
+            systemctl start firewalld
+            if [ $? -eq 0 ];then
+                firewall-cmd --permanent --zone=public --add-port=${Port}/tcp
+                firewall-cmd --permanent --zone=public --add-port=${Port}/udp
+                firewall-cmd --reload
+            else
+                echo "WARNING: Try to start firewalld failed. please enable port ${Port} manually if necessary."
+            fi
+        fi
+    fi
+    echo "firewall set completed..."
+}
+
+if [ `echo $check_port` == '#Port 22' ]; then
+  sed -i "s/#Port 22/Port "{$Port}"/g" $sshd_config_dir
+  firewall_set
+  echo 'Successful'
+elif [ `echo $check_port` == 'Port 22' ]; then
+  sed -i "s/22/"{$Port}"/g" $sshd_config_dir
+  firewall_set
+  echo 'Successful'
+fi
